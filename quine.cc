ec783743a529808592934a320ba885bccd2c630e
MakeNowJust <make.just.on@gmail.com>
2014-03-10 02:33:01 +0900

    Quine in C++11

---
+++ quine.cc
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+void quine(std::vector<std::string> &src) {
+  using namespace std;
+  char quine_str[] = {'Q', 'U', 'I', 'N', 'E', 0};
+  char indent[] = {' ', ' ', ' ', ' ', 0};
+  char q = 34;
+  
+  for (auto s : src) {
+    if (s == quine_str) {
+      for (auto s : src) {
+        cout << indent << q << s << q << ',' << endl;
+      }
+    } else {
+      cout << s << endl;
+    }
+  }
+}
+
+int main(void) {
+  using namespace std;
+  
+  vector<string> src {
+    "#include <string>",
+    "#include <vector>",
+    "#include <iostream>",
+    "",
+    "void quine(std::vector<std::string> &src) {",
+    "  using namespace std;",
+    "  char quine_str[] = {'Q', 'U', 'I', 'N', 'E', 0};",
+    "  char indent[] = {' ', ' ', ' ', ' ', 0};",
+    "  char q = 34;",
+    "  ",
+    "  for (auto s : src) {",
+    "    if (s == quine_str) {",
+    "      for (auto s : src) {",
+    "        cout << indent << q << s << q << ',' << endl;",
+    "      }",
+    "    } else {",
+    "      cout << s << endl;",
+    "    }",
+    "  }",
+    "}",
+    "",
+    "int main(void) {",
+    "  using namespace std;",
+    "  ",
+    "  vector<string> src {",
+    "QUINE",
+    "  };",
+    "  quine(src);",
+    "}",
+  };
+  quine(src);
+}
