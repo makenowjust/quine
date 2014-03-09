818ce62b3af8106c01d40affbf986d67c477b01f
MakeNowJust <make.just.on@gmail.com>
2014-03-10 00:42:54 +0900

    Update to add newline

--- quine.c
+++ quine.c
@@ -1,1 +1,1 @@
-int main(void){char*s="int main(void){char*s=%c%s%c;printf(s,34,s,34);}";printf(s,34,s,34);}
+int main(void){char*s="int main(void){char*s=%c%s%c;printf(s,34,s,34,10);}%c";printf(s,34,s,34,10);}
