f2d3dbd3dee8f0b51e4d695a1960caabaf8b228a
TSUYUSATO Kitsune <make.just.on@gmail.com>
2014-12-26 02:03:51 +0000

    Quine in F#

---
+++ quine.fs
@@ -0,0 +1,16 @@
+[<EntryPoint>]
+let main args =
+  (fun s ->
+    printfn "[<EntryPoint>]
+let main args =
+  (fun s ->
+    printfn %A s s
+  ) %A
+  0" s s
+  ) "[<EntryPoint>]
+let main args =
+  (fun s ->
+    printfn %A s s
+  ) %A
+  0"
+  0
