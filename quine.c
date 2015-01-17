799d6e85d731760473c3bdb9bf8bc5f91bc6d477
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-01-17 04:35:11 +0000

    Quine for C
    remove warnings and success quit

--- quine.c
+++ quine.c
@@ -1,1 +1,2 @@
-int main(void){char*s="int main(void){char*s=%c%s%c;printf(s,34,s,34,10);}%c";printf(s,34,s,34,10);}
+#include<stdio.h>
+int main(void){char*s="#include<stdio.h>%cint main(void){char*s=%c%s%2$c;printf(s,10,34,s);return 0;}%1$c";printf(s,10,34,s);return 0;}
