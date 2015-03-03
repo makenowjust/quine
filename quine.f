945a46d04b8ea6d961311accf625ae616f8b19bd
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-03-03 15:39:43 +0900

    Quine in Fortran 70
    fix string size

--- quine.f
+++ quine.f
@@ -1,5 +1,5 @@
-       character s*999
-       s(  1: 51)=51H#$%character s*999#%j=4#do 400 i=4,450#  if(s(i:i).
+       character s*459
+       s(  1: 51)=51H#$%character s*459#%j=4#do 400 i=4,450#  if(s(i:i).
        s( 52:102)=51HEQ.s(1:1))goto 100#  if(s(i:i).EQ.s(2:2))goto 200# 
        s(103:153)=51H if(s(i:i).EQ.s(3:3))goto 300#  goto 400#  100    w
        s(154:204)=51Hrite(*,1000)'       ',s(j:i-1)$  goto 210#  200    
@@ -7,7 +7,7 @@
        s(256:306)=51H  300    do 310 k=1,450,51$    write(*,2000)k,k+50,
        s(307:357)=51Hs(k:k+50)#  310    continue$  j=i+1#  400  continue
        s(358:408)=51H$ 1000  format(A,A)$ 2000  format('       s(',I3,':
-       s(409:459)=51H',I3,')=51H',A)$stop#end#!(C)2015 TSUYUSATO Kitsune
+       s(409:459)=51H',I3,')=51H',A)$stop#end#(C) 2015 TSUYUSATO Kitsune
        j=4
        do 400 i=4,450
          if(s(i:i).EQ.s(1:1))goto 100
