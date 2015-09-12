faeec6c9f6d5bdbd4f296cd8d9a4a4730975ada2
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-09-12 12:28:05 +0900

    Quine in Rust
    std::fmt's escape syntax was changed

--- quine.rust.rs
+++ quine.rust.rs
@@ -1,3 +1,3 @@
 fn main() {
-  println!("fn main() \\{\n  println!({0:?}, {0:?});\n\\}", "fn main() \\{\n  println!({0:?}, {0:?});\n\\}");
+  println!("fn main() {{\n  println!({0:?}, {0:?});\n}}", "fn main() {{\n  println!({0:?}, {0:?});\n}}");
 }
